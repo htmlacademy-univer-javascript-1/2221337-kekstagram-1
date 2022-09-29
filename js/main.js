67d8f0a4c24fea46737a734e485bdde86541ea3d
Pavel Vakhrin <ratchet23122003@gmail.com>
2022-09-29 20:28:10 +0500

    Больше деталей

--- js/main.js
+++ js/main.js
@@ -1,8 +1,38 @@
-/*Функция возвращает целое число и принимает два параметра:
-нижний (минимальный) и верхний (максимальный) пределы диапозона.
-В теле функции присутствует дестректруризация и возврат отрицательного числа в качестве ошибки
-в случае указания при её вызове аргументов в порядке, не соответствуюшему параметрам функции,
-или в качестве отрицательных чисел соответственно*/
+const MAX_AVATARS_NUMBER = 6;
+const MAX_ITEMS_NUMBER = 25;
+const MIN_LIKES = 15;
+const MAX_LIKES = 200;
+const MAX_COMMENTS_NUMBER = 3;
+
+const MESSAGES = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+];
+
+const NAMES = [
+  'Хейли',
+  'Келли',
+  'Элли',
+  'Григорий',
+  'Никлоай',
+  'Александр'
+];
+
+const DESCRIPTIONS = [
+  'Типичный серый панельный дом',
+  'Кабинет ИРИТ-РтФ с обшарпанной штукатуркой',
+  'Старый чехословацкий трамвай',
+  'Поваленные из-за сильного снегопада деревья',
+  'Огромная очередь в почте',
+  'Расстроенный из-за потерянного пропуска студент'
+];
+
+const imagesItems = [];
+
 const getRandomIntInclusively = (min, max) => {
   min = Math.ceil(min);
   max = Math.floor(max);
@@ -15,11 +45,29 @@ const getRandomIntInclusively = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
-getRandomIntInclusively();
+const getComments = (count) => {
+  const comments = [];
+  for(let i = 1; i <= count; i++){
+    comments.push({
+      id: i,
+      avatar: `img/avatar-${getRandomIntInclusively(1, MAX_AVATARS_NUMBER)}.svg`,
+      message: MESSAGES[getRandomIntInclusively(0, MESSAGES.length - 1)],
+      name: NAMES[getRandomIntInclusively(0, NAMES.length - 1)]
+    });
+  }
+  return comments;
+};
 
-/*Функция возвращает результат проверки длинны строки на максимально доступное значение
-и принимает два параметра: проверяемую строку, длина которой будет сравниваться с максимальным значением,
-и верхний предел (максимальное число).*/
-const checkStringLenght = (string, maxLength) => string.length <= maxLength;
+const getImagesInfo = () => {
+  for(let i = 1; i <= MAX_ITEMS_NUMBER; i++){
+    imagesItems.push({
+      id: i,
+      url: `photos/${i}.jpg`,
+      description: DESCRIPTIONS[getRandomIntInclusively(1, DESCRIPTIONS.length - 1)],
+      likes: getRandomIntInclusively(MIN_LIKES, MAX_LIKES),
+      comments: getComments(getRandomIntInclusively(1, MAX_COMMENTS_NUMBER))
+    });
+  }
+};
 
-checkStringLenght();
+getImagesInfo();
