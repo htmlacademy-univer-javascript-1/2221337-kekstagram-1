0bf38f7fdeb7ecf2ef5ba26af92fbe6e8fb40608
Pavel Vakhrin <ratchet23122003@gmail.com>
2022-10-15 15:01:12 +0500

    Отрисуй меня полностью (часть 2)

--- js/renderer.js
+++ js/renderer.js
@@ -1,12 +1,18 @@
+import { addPictureEvent } from './big-picture.js';
+
 const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
 const pictures = document.querySelector('.pictures');
 const picturesFragment = document.createDocumentFragment();
 
 const renderPicture = (picture) => {
   const newElement = pictureTemplate.cloneNode(true);
-  newElement.src = picture.url;
+
+  newElement.querySelector('.picture__img').src = picture.url;
   newElement.querySelector('.picture__comments').textContent = picture.comments.length;
   newElement.querySelector('.picture__likes').textContent = picture.likes;
+
+  addPictureEvent(newElement, picture);
+
   return newElement;
 };
 
@@ -14,6 +20,7 @@ const renderPictures = (images) => {
   images.forEach((picture) => {
     picturesFragment.appendChild(renderPicture(picture));
   });
+
   pictures.appendChild(picturesFragment);
 };
 
