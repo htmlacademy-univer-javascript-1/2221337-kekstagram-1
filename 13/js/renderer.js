b788e28bb9a417d188f9646de8074f3b245b1183
Keks <keks@htmlacademy.ru>
2022-11-29 18:13:28 +0300

    :heavy_check_mark: Сборка #13

---
+++ 13/js/renderer.js
@@ -0,0 +1,38 @@
+import { addBigPicture } from './big-picture.js';
+
+const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
+const pictures = document.querySelector('.pictures');
+const picturesFragment = document.createDocumentFragment();
+
+const renderPicture = (picture) => {
+  const newElement = pictureTemplate.cloneNode(true);
+
+  newElement.querySelector('.picture__img').src = picture.url;
+  newElement.querySelector('.picture__comments').textContent = picture.comments.length;
+  newElement.querySelector('.picture__likes').textContent = picture.likes;
+
+  newElement.addEventListener('click', (evt) => {
+    evt.preventDefault();
+
+    addBigPicture(picture);
+  });
+
+  return newElement;
+};
+
+const renderPictures = (images) => {
+  images.forEach((picture) => {
+    picturesFragment.appendChild(renderPicture(picture));
+  });
+
+  pictures.appendChild(picturesFragment);
+};
+
+const removePictures = () => {
+  const oldPictures = pictures.querySelectorAll('.picture');
+  oldPictures.forEach((p) => {
+    p.remove();
+  });
+};
+
+export {renderPictures, removePictures};
---
+++ 13/js/upload-form.js
@@ -0,0 +1,77 @@
+import { uploadHashtagInput, clearHashtagsField, checkFormValidation } from './hashtags.js';
+import { isEscapePushed } from './utils.js';
+import { setScale } from './scaler.js';
+import { setEffects } from './effects.js';
+import { setData } from './fetch.js';
+import { addPostMessages, showSuccessMessage, closeMessage, showErrorMessage } from './post-messages.js';
+import { uploadUserPicture} from './user-picture.js';
+
+const form = document.querySelector('.img-upload__form');
+
+const uploadingControl = form.querySelector('#upload-file');
+const uploadingOverlay = form.querySelector('.img-upload__overlay');
+const uploadingClose = form.querySelector('#upload-cancel');
+
+const uploadingComments = uploadingOverlay.querySelector('.text__description');
+const uploadingButton = uploadingOverlay.querySelector('#upload-submit');
+
+const clearForm = () => {
+  uploadingOverlay.classList.add('hidden');
+  document.querySelector('body').classList.remove('modal-open');
+
+  uploadingControl.value = '';
+
+  clearHashtagsField();
+  uploadingComments.value = '';
+
+  closeMessage();
+
+  uploadingButton.disabled = false;
+};
+
+const onEscapeKeyDown = (evt) => {
+  if(isEscapePushed(evt) && !evt.target.classList.contains('text__hashtags') && !evt.target.classList.contains('text__description')) {
+    clearForm();
+
+    document.removeEventListener('keydown', onEscapeKeyDown);
+  }
+};
+
+const closeForm = () => {
+  clearForm();
+
+  document.removeEventListener('keydown', onEscapeKeyDown);
+};
+
+uploadingClose.addEventListener('click', closeForm);
+
+const onUploadClick = () => {
+  document.addEventListener('keydown', onEscapeKeyDown);
+
+  uploadUserPicture(uploadingControl.files[0]);
+
+  uploadingOverlay.classList.remove('hidden');
+  document.querySelector('body').classList.add('modal-open');
+
+  setScale();
+  setEffects();
+
+  uploadHashtagInput();
+};
+
+const uploadForm = () => {
+  uploadingControl.addEventListener('change', onUploadClick);
+  addPostMessages();
+};
+
+const onFormSubmit = (evt) => {
+  evt.preventDefault();
+
+  if(checkFormValidation()) {
+    setData(showSuccessMessage, showErrorMessage, 'POST', new FormData(form));
+  }
+};
+
+form.addEventListener('submit', onFormSubmit);
+
+export{uploadForm, closeForm, onEscapeKeyDown};
